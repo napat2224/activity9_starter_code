8c389ae7622c71e59b83119e8e6cc89bde184492
boomenghub <12evolte@gmail.com>
2024-04-18 21:30:19 +0700

    can login and register

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -8,8 +8,7 @@ router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
-        //const salt = await bcrypt.genSalt(10);
-        //req.body.password = await bcrypt.hash(req.body.password, salt);
+        
       } catch (err) {
         return res.status(500).json(err);
       }
