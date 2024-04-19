cf2f6dfcdf87459094399cfd24b372a591c3410f
boomenghub <12evolte@gmail.com>
2024-04-19 15:07:23 +0700

    update confirmpassw

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -6,26 +6,38 @@ import User from "../models/Users.js";
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
+    //check pass
+    if(req.body.password == req.body.confirmpassword){
+        const user = await User.findOne({ username: req.body.username });
+        if(!user){
+          //create new user
+          const newUser = new User({
+            username: req.body.username,
+            password: req.body.password,
+          });
+  
+          //save user and respond
+          user = await newUser.save();
+          console.log(user)
+          res.status(200).json(user);
+        }else{
+          res.status(400).json("username already use");
+        }
+          
+    }else{
+      res.status(400).json("password not match");
+    }
     
-    //create new user
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    //save user and respond
-    const user = await newUser.save();
-    res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    //res.status(400).json("cant create acc");
+    res.status(400).json(err)
   }
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ username: req.body.username });
     if(!user){
         res.status(400).json("user not found");
         return ;
