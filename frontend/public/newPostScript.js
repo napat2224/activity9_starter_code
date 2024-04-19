740072c3471a55cfff5780aebb7126f5710deefc
chonlaprim <primsrisanan@gmail.com>
2024-04-19 17:22:13 +0700

    can select photo and preview

---
+++ frontend/public/newPostScript.js
@@ -0,0 +1,18 @@
+document.getElementById('photo_file').addEventListener('change', function(event) {
+    var file = event.target.files;
+    var preview = document.getElementById('previewPic');
+    
+    // Clear any existing content
+    preview.innerHTML = '';
+    
+    var img = document.createElement('img');
+    img.src = URL.createObjectURL(file[0]);
+    img.style.width = '100%';
+    img.style.height = 'auto';
+    img.style.maxHeight = '430px'
+    img.style.alignSelf = 'center';
+    img.style.verticalAlign = 'middle';
+
+    // Append the container to the preview div
+    preview.appendChild(img);
+})
