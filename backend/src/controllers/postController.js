00903e0aab9e73b40ad06857b4dc17fc2bf5cee9
Ubuntu <ubuntu@ip-172-31-62-69.ec2.internal>
2024-04-23 07:46:07 +0000

    No cors res set

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -27,20 +27,10 @@ export const likeDisLikePost = async (req, res) => {
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
       post.likeNumber = post.likes.length;
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
       res.status(200).json("The post has been liked");
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
       post.likeNumber = post.likes.length;
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
       res.status(200).json("The post has been disliked");
     }
   } catch (err) {
